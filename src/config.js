c3a5df791e1c03d91eb9eb4599ab9517de0c65af
Rytis Perednikas <prrytis@gmail.com>
2021-04-14 13:58:53 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@ import transenv from 'transenv'
 
 export default transenv()(({str, bool}) => {
   return {
-    isHttps: bool('HTTPS'),
-    port: str('PORT'),
-    ghRepo: str('GH_REPO'),
-    ssoUrl: str('VL_SSO_URL'),
-    ssoKey: str('VL_SSO_KEY'),
-    ghApiUrl: `${str('VL_SSO_URL')}/api/github`,
-    emsKey: str('EMS_KEY'),
-    pandadocKey: str('PANDADOC_KEY'),
+//     isHttps: bool('HTTPS'),
+//     port: str('PORT'),
+//     ghRepo: str('GH_REPO'),
+//     ssoUrl: str('VL_SSO_URL'),
+//     ssoKey: str('VL_SSO_KEY'),
+//     ghApiUrl: `${str('VL_SSO_URL')}/api/github`,
+//     emsKey: str('EMS_KEY'),
+//     pandadocKey: str('PANDADOC_KEY'),
 
     /*google: {
       email: str('GOOGLE_EMAIL'),
